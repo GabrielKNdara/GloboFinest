cdb5bd642b6050a1b2030bba2a4a9c15a9ac3e4d
Gabriel Ndara <gabrielndara@gmail.com>
2023-03-14 22:03:47 +0200

    first commit

---
+++ lib/shared/menu_drawer.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import '../screens/bmi_screen.dart';
+import '../screens/intro_screen.dart';
+
+class MenuDrawer extends StatelessWidget {
+  const MenuDrawer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: builderMenuItems(context),
+      ),
+    );
+  }
+  List<Widget> builderMenuItems(BuildContext context){
+    final List<String> menuTitles = [
+      'Home',
+      'BMI Calculator',
+      'Weather',
+      'Training'
+    ];
+    List<Widget> menuItems = [];
+    menuItems.add(DrawerHeader(
+        decoration: BoxDecoration(color: Colors.blueGrey),
+    child: Text('Globo Fitness',
+    style: TextStyle(color: Colors.white,fontSize: 28),),));
+    menuTitles.forEach((String element) {
+      Widget screen = Container();
+      menuItems.add(ListTile(
+        title: Text(element,
+        style: TextStyle(fontSize: 18),),
+        onTap: (){
+          switch(element){
+            case 'Home':
+              screen = IntroScreen();
+              break;
+            case 'BMI Calculator':
+              screen = BmiScreen();
+              break;
+
+          }
+          Navigator.of(context).pop();
+          Navigator.of(context).push(
+            MaterialPageRoute(builder: (context) => screen)
+          );
+
+        },
+      ));
+    });
+    return menuItems;
+  }
+}
+
